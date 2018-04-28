2aad880612bd7d09e6815343039cb459e4afcc77
huntersheston <huntersmauelheston@gmail.com>
2018-04-28 11:05:51 -0400

    login functions added

---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDsD3ZgnIQhlhbMDt5J2_isAvgYMk4Ugvc",
+    authDomain: "groupup-c482e.firebaseapp.com",
+    databaseURL: "https://groupup-c482e.firebaseio.com",
+    projectId: "groupup-c482e",
+    storageBucket: "groupup-c482e.appspot.com",
+    messagingSenderId: "18796911954"
+};
+
+if(!firebase.apps.length){
+    firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export {
+    auth,
+};
