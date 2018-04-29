1f5f0268b3b0a9dbc44fc7e77d3e4b6aa54a5df1
Hunter Heston <hunter_heston@yahoo.com>
2018-04-29 19:11:27 -0400

    context component now used for auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Route,
+    BrowserRouter as Router,
+    Route,
 } from 'react-router-dom';
+import withAuthentication from './withAuthentication';
+
 
 import Navigation from './Navigation';
 import LandingPage from './Landing';
@@ -15,37 +17,19 @@ import AccountPage from './Account';
 import * as routes from '../constants/routes';
 
 const App = () =>
-  <Router>
-    <div>
-      <Navigation />
+    <Router>
+        <div>
+            <Navigation />
 
-      <hr />
+            <hr/>
 
-      <Route
-        exact path={routes.LANDING}
-        component={() => <LandingPage />}
-      />
-      <Route
-        exact path={routes.SIGN_UP}
-        component={() => <SignUpPage />}
-      />
-      <Route
-        exact path={routes.SIGN_IN}
-        component={() => <SignInPage />}
-      />
-      <Route
-        exact path={routes.PASSWORD_FORGET}
-        component={() => <PasswordForgetPage />}
-      />
-      <Route
-        exact path={routes.HOME}
-        component={() => <HomePage />}
-      />
-      <Route
-        exact path={routes.ACCOUNT}
-        component={() => <AccountPage />}
-      />
-    </div>
-  </Router>
+            <Route exact path={routes.LANDING} component={() => <LandingPage />} />
+            <Route exact path={routes.SIGN_UP} component={() => <SignUpPage />} />
+            <Route exact path={routes.SIGN_IN} component={() => <SignInPage />} />
+            <Route exact path={routes.PASSWORD_FORGET} component={() => <PasswordForgetPage />} />
+            <Route exact path={routes.HOME} component={() => <HomePage />} />
+            <Route exact path={routes.ACCOUNT} component={() => <AccountPage />} />
+        </div>
+    </Router>
 
-export default App;
+export default withAuthentication(App);
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,22 +3,26 @@ import { Link } from 'react-router-dom';
 
 import SignOutButton from './SignOut'
 import * as routes from '../constants/routes';
+import AuthUserContext from './AuthUserContext';
 
-const Navigation = ({ authUser }) =>
-    <div>
-        { authUser
-            ? <NavigationAuth />
-            : <NavigationNonAuth />
+const Navigation = () =>
+    <AuthUserContext.Consumer>
+        {authUser => authUser
+            ? <NavigationAuth/>
+            : <NavigationNonAuth/>
         }
-    </div>
+    </AuthUserContext.Consumer>
 
-const NavigationAuth = () =>
-    <ul>
-        <li><Link to={routes.LANDING}>Landing</Link></li>
-        <li><Link to={routes.HOME}>Home</Link></li>
-        <li><Link to={routes.ACCOUNT}>Account</Link></li>
-        <li><SignOutButton /></li>
-    </ul>
+
+        const NavigationAuth = () =>
+    <div>
+        <ul>
+            <li><Link to={routes.LANDING}>Landing</Link></li>
+            <li><Link to={routes.HOME}>Home</Link></li>
+            <li><Link to={routes.ACCOUNT}>Account</Link></li>
+            <li><SignOutButton /></li>
+        </ul>
+    </div>
 
 const NavigationNonAuth = () =>
     <ul>
