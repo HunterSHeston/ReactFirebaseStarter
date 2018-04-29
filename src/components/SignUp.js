34ef5dbbcaa6cb3de71849606e1f483876386dbc
Hunter Heston <hunter_heston@yahoo.com>
2018-04-29 11:15:01 -0400

    finished signup

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import {
+    Link,
+    withRouter,
+} from 'react-router-dom';
 
 import { auth} from "../firebase";
 import * as routes from '../constants/routes';
 
-const SignUpPage = () =>
+const SignUpPage = ({ history }) =>
   <div>
     <h1>Sign Up</h1>
-    <SignUpForm />
+    <SignUpForm history={history} />
   </div>
 
 const INITIAL_STATE = {
@@ -37,11 +40,16 @@ class SignUpForm extends Component
           username,
           email,
           passwordOne,
-    } = this.state;
+      } = this.state;
+
+      const {
+          history,
+      } = this.props;
 
       auth.doCreateUserWithEmailAndPassword(email, passwordOne)
           .then(authUser => {
               this.setState(() => ({...INITIAL_STATE}));
+              history.push(routes.HOME);
           })
           .catch(error => {
               this.setState(byPropKey('error', error));
@@ -112,7 +120,7 @@ const SignUpLink = () =>
     <Link to={routes.SIGN_UP}>Sign Up</Link>
   </p>
 
-export default SignUpPage;
+export default withRouter(SignUpPage);
 
 export {
   SignUpForm,
