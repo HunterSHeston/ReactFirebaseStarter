d9502ae3d5d51e9349f998ef63f36ffc9bd7e4d3
Hunter Heston <hunter_heston@yahoo.com>
2018-05-08 17:45:30 -0400

    fixed list of users on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -36,10 +36,11 @@ const UserList = ({ users }) =>
     <div>
         <h2>List of Usernames of Users</h2>
         <p>(Saved on Sign Up in Firebase Database)</p>
-
+        <ul>
         {Object.keys(users).map(key =>
-            <div key={key}>{users[key].username}</div>
+            <li key={key}>{users[key].username}</li>
         )}
+        </ul>
     </div>
 
 const authCondition = (authUser) => !!authUser;
